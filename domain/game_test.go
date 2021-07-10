7751bc4b0db53fe82a8048535f5bad74050b6f2e
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2021-07-10 16:17:08 +0300

    test for update game

--- domain/game_test.go
+++ domain/game_test.go
@@ -8,8 +8,10 @@ import (
 
 var gameID = GenerateGameID()
 var wordID = GenerateWordID()
+var anotherWordID = GenerateWordID()
 var attemptCount = 5
 var word = NewWord(wordID, "hello")
+var anotherWord = NewWord(anotherWordID, "world")
 
 func TestNewGame(t *testing.T) {
 	t.Run("normally create game", func(t *testing.T) {
@@ -32,5 +34,26 @@ func TestNewGame(t *testing.T) {
 		_, err := NewGame(gameID, &word, -1)
 
 		assert.Equal(t, err, ErrNegativeAttemptCount)
+	})
+}
+
+func TestUpdateGame(t *testing.T) {
+	t.Run("normally update game", func(t *testing.T) {
+
+		game, err := NewGame(gameID, &word, attemptCount)
+		assert.Equal(t, err, nil)
+
+		err = UpdateGame(game, SetAttemptCount(10))
+		assert.Equal(t, err, nil)
+		assert.Equal(t, game.AttemptCount, 10)
+	})
+
+	t.Run("normally update word", func(t *testing.T) {
+		game, err := NewGame(gameID, &word, attemptCount)
+		assert.Equal(t, err, nil)
+
+		err = UpdateGame(game, SetWord(&anotherWord))
+		assert.Equal(t, err, nil)
+		assert.Equal(t, game.Word.String(), anotherWord.String())
 	})
 }
